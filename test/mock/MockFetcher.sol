5ae403c21714a3f10dca12e0c211033119f947da
immaxkent <zmkd@solacelabs.org>
2025-08-25 16:07:34 +0100

    fix(contracts): adding missing function signatures

--- test/mock/MockFetcher.sol
+++ test/mock/MockFetcher.sol
@@ -20,6 +20,11 @@ abstract contract AMockFetcher is IUniversalDexInterface {
     function getPoolAddress(address, address) external pure override returns (address) {
         return address(0);
     }
+
+    function getPrice(address tokenIn, address tokenOut, uint256 amountIn) pure view override returns (uint256) {
+        // Return a mock price - simple 1:1 ratio for testing
+        return amountIn;
+    }
 }
 
 contract MockFetcher1 is AMockFetcher {
